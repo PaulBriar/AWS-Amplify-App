842a3a215661c43dcdd5c9789927b5025224ba55
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-03-22 09:52:58 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            We're Amplified!!
           </a>
         </header>
       </div>
